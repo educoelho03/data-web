9a7582f10c9a60d8d78ca66f33e152a25f6a5603
Eduardo Paiva Coelho <ecoelho@constanciainvest.com.br>
2024-11-18 09:42:22 -0300

    add new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios';
+
 import './styles/globalStyles.css'
 import Navbar from './components/Navbar'
 import PageTitle from './components/PageTitle'
 
 function App() {
+  const [data, setData] = useState([])
+
+  async function fetchData(){
+      try {
+          const response = await axios.get('/api/data')
+          setData(response)
+      } catch(error){
+          console.error("Error fetching Data", error)
+      }
+  }
+
+  useEffect(() => {
+      fetchData()
+  }, [])
+
   return (
     <div>
       <Navbar />
