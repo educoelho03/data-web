cdfb51b90373c7d6bb869c294095210804e0ed6b
Eduardo Paiva Coelho <ecoelho@constanciainvest.com.br>
2024-11-18 10:00:16 -0300

    add css

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+import '../styles/dataTable.css'
 
 const DataTable = () => {
     const [data, setData] = useState([]);
